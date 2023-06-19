d5e136db296b4ca42912790bf5f7faed2773ce27
CAMARA <88945535+camara92@users.noreply.github.com>
2023-06-20 01:22:30 +0200

    affichage de la date et transformation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Text, StyleSheet, View, TextInput } from 'react-native'
 
+const days = ["Dimanche", "Lundi", "MArdi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
+const month= ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
 
 export default function Header() {
+    const date = new Date(); 
   return (
-    <Text> {Date.now().toString()} </Text>
+    <Text> {`${days[date.getDay()]} ${date.getDate()} ${month[date.getMonth()] } ${[date.getFullYear()] } `} </Text>
+    // <Text> {Date.now().toString()} </Text>
   )
 }
+
+
+
+
+const styles = StyleSheet.create({
+
+})
